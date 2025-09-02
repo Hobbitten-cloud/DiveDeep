f2f26f75a1544dced3aa44c30f294bf0548cc760
Albert <Alberttrabergmoth@gmail.com>
2025-09-02 11:46:46 +0200

    skibidi billede

--- DiveDeepProject/Persistence/CategoriesRepository.cs
+++ DiveDeepProject/Persistence/CategoriesRepository.cs
@@ -6,13 +6,13 @@ namespace DiveDeepProject.Persistence
     {
         private static List<Category> categories = new List<Category>
         {
-            new Category { Id = 1, Name = "Dykkersæt", ImagePath = "" },
-            new Category { Id = 2, Name = "Snorkelsæt", ImagePath = "" },
+            new Category { Id = 1, Name = "Dykkersæt", ImagePath = "lib/Public/DivingGearSet.png" },
+            new Category { Id = 2, Name = "Snorkelsæt", ImagePath = "lib/Public/SnorkelSet.png" },
             new Category { Id = 3, Name = "BCD", ImagePath = "lib/Public/BCD.png" },
             new Category { Id = 4, Name = "Dykkerdragter", ImagePath = "lib/Public/BCD.png" },
-            new Category { Id = 5, Name = "Tanke", ImagePath = "" },
-			new Category { Id = 6, Name = "Regulatorsæt", ImagePath = "" },
-            new Category { Id = 7, Name = "Maske/snorkel", ImagePath = "" },
+            new Category { Id = 5, Name = "Tanke", ImagePath = "lib/Public/DivingTank.png" },
+			new Category { Id = 6, Name = "Regulatorsæt", ImagePath = "lib/Public/Regulator.png" },
+            new Category { Id = 7, Name = "Maske/snorkel", ImagePath = "lib/Public/DivingMask-Snorkel.png" },
             new Category { Id = 8, Name = "Finner", ImagePath = "lib/Public/DivingFins.png" }
         };
 
