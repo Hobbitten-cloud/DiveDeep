49039b24cba30eee013b53db59d25db08ee6935f
Søren Frederik Ploug <soren.ploug04@gmail.com>
2025-09-03 14:13:53 +0200

    Fixed category repo

--- DiveDeepProject/Persistence/Repo/CategoryRepo.cs
+++ DiveDeepProject/Persistence/Repo/CategoryRepo.cs
@@ -11,15 +11,15 @@ namespace DiveDeepProject.Persistence.Repo
         public CategoryRepo()
         {
             _categories = new List<Category>();
-            
-            new Category { Id = 1, Name = "Dykkersæt", ImagePath = "lib/Public/DivingGearSet.png" },
-            new Category { Id = 2, Name = "Snorkelsæt", ImagePath = "lib/Public/SnorkelSet.png" },
-            new Category { Id = 3, Name = "BCD", ImagePath = "lib/Public/BCD.png" },
-            new Category { Id = 4, Name = "Dykkerdragter", ImagePath = "lib/Public/BCD.png" },
-            new Category { Id = 5, Name = "Tanke", ImagePath = "lib/Public/DivingTank.png" },
-			new Category { Id = 6, Name = "Regulatorsæt", ImagePath = "lib/Public/Regulator.png" },
-            new Category { Id = 7, Name = "Maske/snorkel", ImagePath = "lib/Public/DivingMask-Snorkel.png" },
-            new Category { Id = 8, Name = "Finner", ImagePath = "lib/Public/DivingFins.png" }
+
+            Create(new Category { Id = 1, Name = "Dykkersæt", ImagePath = "lib/Public/DivingGearSet.png" });
+            Create(new Category { Id = 2, Name = "Snorkelsæt", ImagePath = "lib/Public/SnorkelSet.png" });
+            Create(new Category { Id = 3, Name = "BCD", ImagePath = "lib/Public/BCD.png" });
+            Create(new Category { Id = 4, Name = "Dykkerdragter", ImagePath = "lib/Public/BCD.png" });
+            Create(new Category { Id = 5, Name = "Tanke", ImagePath = "lib/Public/DivingTank.png" });
+            Create(new Category { Id = 6, Name = "Regulatorsæt", ImagePath = "lib/Public/Regulator.png" });
+            Create(new Category { Id = 7, Name = "Maske/snorkel", ImagePath = "lib/Public/DivingMask-Snorkel.png" });
+            Create(new Category { Id = 8, Name = "Finner", ImagePath = "lib/Public/DivingFins.png" });
         }
 
         public Category Create(Category item, SortingService sortingService)
