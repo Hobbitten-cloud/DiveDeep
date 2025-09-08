634fc96840a28b70696c899c8ea0bf2d7364c58f
Daniel Nielsen <danielarnbjerg@hotmail.dk>
2025-09-08 16:19:51 +0200

    AboutUs and ContactUs pages done

---
+++ DiveDeepProject/Controllers/TermsOfConditionsOfSaleAndDeliveryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeepProject.Controllers
+{
+    public class TermsOfConditionsOfSaleAndDeliveryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
