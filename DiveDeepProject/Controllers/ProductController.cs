75c7f25bdf97c6d76e3f3c49b98512a02e3f5326
Albert <Alberttrabergmoth@gmail.com>
2025-09-09 11:46:47 +0200

    s,m,l dropdown virker også

--- DiveDeepProject/Controllers/ProductController.cs
+++ DiveDeepProject/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using DiveDeepProject.Persistence;
 using DiveDeepProject.Persistence.IRepo;
 using DiveDeepProject.Persistence.Repo;
 using DiveDeepProject.Models.Inferfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DiveDeepProject.Models.Enums;
 
 namespace DiveDeepProject.Controllers
 {
@@ -27,6 +29,7 @@ namespace DiveDeepProject.Controllers
 
         public IActionResult Details(int id)
         {
+
             if (_prodRepo is ProductRepo repo)
             {
                 var product = repo.Get(id);
@@ -34,7 +37,17 @@ namespace DiveDeepProject.Controllers
                 {
                     return NotFound();
                 }
-                return View(product);
+				// Prepare filtered enum list (only S, M, L)
+				var allowedSizes = new[] { Size.S, Size.M, Size.L };
+				var filteredList = allowedSizes.Select(s => new SelectListItem
+				{
+					Text = s.ToString(),          // or get display attribute if you have one
+					Value = ((int)s).ToString()
+				}).ToList();
+
+				ViewData["SizeOptions"] = filteredList;
+
+				return View(product);
             }
             return NotFound();
         }
