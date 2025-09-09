e5ad09463f50414e24fe99ba88aded5d35e5a6d3
Hobbitten <janenhansen@gmail.com>
2025-09-09 09:38:50 +0200

    Add enum to help display a gender

---
+++ DiveDeepProject/Controllers/PackageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeepProject.Controllers
+{
+    public class PackageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DiveDeepProject/Models/Enums/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DiveDeepProject.Models.Enums
+{
+	public enum Gender
+	{
+		[Display(Name = "Herre")]
+		Male,
+
+		[Display(Name = "Dame")]
+		Female
+	}
+}
