0ac113b8aac4c209ad24f8040a8cea19e1e55950
Daniel Nielsen <danielarnbjerg@hotmail.dk>
2025-09-09 11:21:36 +0200

    DivingSuit size added

---
+++ DiveDeepProject/ViewModels/PackageProductViewData.cs
@@ -0,0 +1,16 @@
+﻿using DiveDeepProject.Models;
+using DiveDeepProject.Models.Inferfaces;
+
+namespace DiveDeepProject.ViewModels
+{
+    public class PackageProductViewData
+    {
+        public Package package;
+        public BCD bcd;
+        public Regulatorset regulatorset;
+        public DivingSuit divingSuit;
+        public Flipper flipper;
+        public SnorkelSet snorkelSet;
+        public Tank tank;
+    }
+}
