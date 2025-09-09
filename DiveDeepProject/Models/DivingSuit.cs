e3d041ba6bc0a4f7c07f531ce27ce6e16f742c83
Albert <Alberttrabergmoth@gmail.com>
2025-09-09 11:54:05 +0200

    size = null

--- DiveDeepProject/Models/DivingSuit.cs
+++ DiveDeepProject/Models/DivingSuit.cs
@@ -9,7 +9,7 @@ namespace DiveDeepProject.Models
         public string Description { get; set; }
         public string Brand { get; set; }
         public double PricePerDay { get; set; }
-        public Size Size { get; set; }
+        public Size? Size { get; set; }
         public string Type { get; set; }
         public string Gender { get; set; }
         public string? Thickness { get; set; }
--- DiveDeepProject/Models/Flipper.cs
+++ DiveDeepProject/Models/Flipper.cs
@@ -32,7 +32,7 @@ namespace DiveDeepProject.Models
         public string Description { get; set; }
         public string Brand { get; set; }
         public double PricePerDay { get; set; }
-        public Size Size { get; set; }
+        public Size? Size { get; set; }
         public string Model { get; set; }
         public string ImagePath { get; set; } = "lib/Public/DesignImageTemplate.png";
         public Flipper() 
