92d684beb0aef062eef76a7059ff8376b67cba8b
Daniel Nielsen <danielarnbjerg@hotmail.dk>
2025-09-12 11:14:49 +0200

    Started making the database

---
+++ DiveDeepProject/Data/DiveDeepContext.cs
@@ -0,0 +1,23 @@
+﻿using DiveDeepProject.Models.Domain;
+using DiveDeepProject.Models.Inferfaces:
+using Microsoft.EntityFrameworkCore;
+
+namespace DiveDeepProject.Data
+{
+    public class DiveDeepContext
+    {
+        public DbSet<BCD> BCDs { get; set; }
+        public DbSet<DivingSuit> DivingSuits { get; set; }
+        public DbSet<Flipper> Flippers { get; set; }
+        public DbSet<Regulatorset> Regulatorsets { get; set; }
+        public DbSet<SnorkelSet> SnorkelSets { get; set; }
+        public DbSet<Tank> Tanks { get; set; }
+        public DbSet<IProduct> IProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IProduct>()
+                .HasOne<BCD>(b => b.BCD)
+        }
+    }
+}
