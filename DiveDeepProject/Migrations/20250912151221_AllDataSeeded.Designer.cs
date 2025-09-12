cabe88b34baf8d764c9d52fe1e6555dccee9005c
Hobbitten-cloud <janenhansen@gmail.com>
2025-09-12 17:16:14 +0200

    Fixed so now you can use migrations to setup the full seed for the database. Name it - DiveDeep

--- DiveDeepProject/Migrations/20250912151221_AllDataSeeded.Designer.cs
+++ DiveDeepProject/Migrations/20250912151221_AllDataSeeded.Designer.cs
@@ -3,6 +3,7 @@ using DiveDeepProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DiveDeepProject.Migrations
 {
     [DbContext(typeof(DiveDeepContext))]
-    partial class DiveDeepContextModelSnapshot : ModelSnapshot
+    [Migration("20250912151221_AllDataSeeded")]
+    partial class AllDataSeeded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
