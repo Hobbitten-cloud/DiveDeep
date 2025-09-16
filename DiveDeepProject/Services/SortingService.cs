29ceb0e1a1ef97f12c68069692d0170879a6c5a8
Hobbitten-cloud <janenhansen@gmail.com>
2025-09-16 10:02:30 +0200

    Indentation fixes

--- DiveDeepProject/Services/SortingService.cs
+++ DiveDeepProject/Services/SortingService.cs
@@ -5,49 +5,51 @@ namespace DiveDeepProject.Services
 {
     public class SortingService : IService //This service is for handling logick surrounding sorting products by category
     {
-
         private readonly ProductRepo _productRepo;
         public SortingService(ProductRepo productRepo)
         {
             _productRepo = productRepo;
-        }   
+        }
+
         public List<Product> SortProductsByCategory(Category item)
         {
             try
             {
                 List<Product> sortedProducts = new List<Product>();
+
                 if (item.Name == "BCD") // BCD
                 {
-                     sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(BCD)).ToList();
+                    sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(BCD)).ToList();
                 }
 
                 else if (item.Name == "Maske/snorkel") //SnorkelSet
                 {
-                     sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(SnorkelSet)).ToList();
+                    sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(SnorkelSet)).ToList();
                 }
 
-               else if (item.Name == "Regulatorsæt") //Regulatorset
+                else if (item.Name == "Regulatorsæt") //Regulatorset
                 {
-                     sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(Regulatorset)).ToList();
+                    sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(Regulatorset)).ToList();
                 }
 
                 else if (item.Name == "Tanke") //Tank
                 {
-                     sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(Tank)).ToList();
+                    sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(Tank)).ToList();
                 }
 
-                else if(item.Name == "Finner") //Flippers
+                else if (item.Name == "Finner") //Flippers
                 {
                     sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(Flipper)).ToList();
                 }
 
                 else if (item.Name == "Dykkerdragter") //DivingSuit
                 {
-                     sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(DivingSuit)).ToList();
+                    sortedProducts = _productRepo.GetAll().Where(p => p.GetType() == typeof(DivingSuit)).ToList();
                 }
                 return sortedProducts;
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 Console.WriteLine(ex.Message);
                 return _productRepo.GetAll();
             }
