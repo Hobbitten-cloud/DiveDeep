12eaf9cf39d14342bf22043247b7a88ade452247
Albert <alberttrabergmoth@gmail.com>
2025-09-19 13:33:07 +0200

    default role is User

--- DiveDeepProject/Program.cs
+++ DiveDeepProject/Program.cs
@@ -51,7 +51,7 @@ namespace DiveDeepProject
             {
                 var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-                var roles = new[] { "Admin", "Member" };
+                var roles = new[] { "Admin", "User" };
 
                 foreach (var role in roles)
                 {
