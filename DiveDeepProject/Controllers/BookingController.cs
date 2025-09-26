3c3ec4f4276b452d191719f7ec07fe5c4a819048
Dennis <dttr67383@edu.ucl.dk>
2025-09-26 10:00:13 +0200

    Booking lavet lidt på mangler rigtig data

---
+++ DiveDeepProject/Controllers/BookingController.cs
@@ -0,0 +1,15 @@
+﻿using DiveDeepProject.Models.Domain;
+using DiveDeepProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeepProject.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View(new List<Receipt>());
+        }
+    }
+}
