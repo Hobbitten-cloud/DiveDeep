2d274b267008f43910209bfe9baa2c7f63fb94a4
Søren <Soren.ploug04@gmail.com>
2025-09-26 11:45:34 +0200

    commit

--- DiveDeepProject/Program.cs
+++ DiveDeepProject/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace DiveDeepProject
 {
     public class Program
-    {
+    {//
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
