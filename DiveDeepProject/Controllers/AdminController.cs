ebf7c87750396433fe5333ce9665dc2a5b81558e
Daniel Nielsen <danielarnbjerg@hotmail.dk>
2025-10-07 08:58:15 +0200

    ManageProducts site setup begun

---
+++ DiveDeepProject/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeepProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult ManageProducts()
+        {
+            return View();
+        }
+    }
+}
