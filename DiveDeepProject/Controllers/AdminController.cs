88029e4dd5e5f9fa89777a0205b4c8d4bceecaeb
Hobbitten <janenhansen@gmail.com>
2025-10-07 11:21:57 +0200

    Push updates

--- DiveDeepProject/Controllers/AdminController.cs
+++ DiveDeepProject/Controllers/AdminController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiveDeepProject.Models.Domain;
+using DiveDeepProject.Persistence.IRepo;
+using DiveDeepProject.Persistence.Repo;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiveDeepProject.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IRepo<Product> _prodRepo;
+        private readonly IRepo<Package> _packageRepo;
+
+        public AdminController(ProductRepo prodRepo, PackageRepo packageRepo)
+        {
+            _prodRepo = prodRepo;
+            _packageRepo = packageRepo;
+        }
+
         public IActionResult ManageProducts()
         {
+            if (_prodRepo is ProductRepo repo)
+            {
+                var products = repo.GetAll();
+                return View(products);
+            }
+
+            return View();
+        }
+
+        public IActionResult CreateProduct()
+        {
+
+            return View();
+        }
+
+        public IActionResult EditProduct(int Id)
+        {
+
             return View();
         }
     }
