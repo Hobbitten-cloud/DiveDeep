dad3f2f6d40ad87db82c58746908aaea22287c9e
Daniel Nielsen <danielarnbjerg@hotmail.dk>
2025-10-10 12:14:49 +0200

    Push

--- DiveDeepProject.API/Controllers/ProductAPIController.cs
+++ DiveDeepProject.API/Controllers/ProductAPIController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiveDeepProject.Persistence.Repo;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiveDeepProject.API.Controllers
 {
-    public class ProductAPIController : Controller
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
     {
+        private readonly ProductRepo _prodRepo;
+
+        public ProductAPIController(ProductRepo prodRepo)
+        {
+            _prodRepo = prodRepo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
         }
     }
 }
