a30c1e0e5b9641b67e6a034875b6fd119c23bcd9
Hobbitten-cloud <janenhansen@gmail.com>
2025-10-10 13:33:58 +0200

    Finished adding CORS

--- DiveDeepProject.API/Program.cs
+++ DiveDeepProject.API/Program.cs
@@ -1,6 +1,10 @@
 
 using DiveDeepProject.Data;
+using DiveDeepProject.Models.Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using System.Text.Json.Serialization;
 
 namespace DiveDeepProject.API
@@ -23,15 +27,43 @@ namespace DiveDeepProject.API
                 options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
             });
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Add Swagger
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
 
             builder.Services.AddDbContext<DiveDeepContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("MyDBConnection"));
             });
 
+            // Configure Identity and Authentication
+            builder.Services.AddAuthentication();
+            builder.Services.AddIdentityApiEndpoints<ApplicationUser>().AddEntityFrameworkStores<DiveDeepContext>();
+
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", policy =>
+                {
+                    policy
+                    // Only allow these origins
+                    .WithOrigins("https://localhost:7280")
+                    .WithOrigins("https://localhost:9999")
+                    // Only allow these request headers
+                    .WithHeaders("Content-Type", "Authorization", "Accept")
+                    // Only allow these HTTP methods
+                    .WithMethods("GET", "POST", "PUT", "DELETE");
+                });
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -40,9 +72,13 @@ namespace DiveDeepProject.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
 
+            // Identity and Authentication
+            app.MapIdentityApi<ApplicationUser>();
+
+            // Apply the named CORS policy globally
+            app.UseCors("AllowSpecificOrigin");
             app.UseAuthorization();
 
             app.MapControllers();
