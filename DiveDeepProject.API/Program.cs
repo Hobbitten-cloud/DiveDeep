c2c265576e880bb58f884b091e6d15cc81f5febb
Hobbitten-cloud <janenhansen@gmail.com>
2025-10-10 13:06:20 +0200

    Init branch

--- DiveDeepProject.API/Program.cs
+++ DiveDeepProject.API/Program.cs
@@ -11,6 +11,7 @@ namespace DiveDeepProject.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
             // Add services to the container.
             builder.Services.AddDbContext<DiveDeepContext>(options =>
             {
