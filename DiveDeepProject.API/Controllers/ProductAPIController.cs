50c121a81524ab45a7685ff407b420a4483875a4
Daniel Nielsen <danielarnbjerg@hotmail.dk>
2025-10-10 12:02:39 +0200

    Push til Nicklas

---
+++ DiveDeepProject.API/Controllers/ProductAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeepProject.API.Controllers
+{
+    public class ProductAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
